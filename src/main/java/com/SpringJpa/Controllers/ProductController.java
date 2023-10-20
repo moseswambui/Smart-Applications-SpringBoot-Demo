f82caf58dadf902cf06b5e5bcad2c6f9cb748456
moses wambui <moses.wambui@smart.local>
2023-10-20 07:51:47 +0300

    Properties File - Change DB options, product Controller POST implementation

--- src/main/java/com/SpringJpa/Controllers/ProductController.java
+++ src/main/java/com/SpringJpa/Controllers/ProductController.java
@@ -1,5 +1,23 @@
 package com.SpringJpa.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringJpa.Entities.ProductEntity;
+import com.SpringJpa.Repositories.ProductRepository;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
     
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Object createProduct(@RequestBody ProductEntity productEntity){
+        return productRepository.save(productEntity);
+    }
 }
--- src/main/java/com/SpringJpa/Entities/ProductEntity.java
+++ src/main/java/com/SpringJpa/Entities/ProductEntity.java
@@ -3,6 +3,7 @@ package com.SpringJpa.Entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
