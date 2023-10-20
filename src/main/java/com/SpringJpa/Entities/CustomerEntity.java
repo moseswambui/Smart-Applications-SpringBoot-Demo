5bf08f44afc014528c260e71539dab00c313a0a1
moses wambui <moses.wambui@smart.local>
2023-10-20 08:15:23 +0300

    Adding Patching Customer Entity - Changes DB settings in application.properties

--- src/main/java/com/SpringJpa/Entities/CustomerEntity.java
+++ src/main/java/com/SpringJpa/Entities/CustomerEntity.java
@@ -3,6 +3,8 @@ package com.SpringJpa.Entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 
 @Entity
