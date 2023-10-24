05196d51bcfb5397a3af442fac6d3f137c88ba0e
moses wambui <moses.wambui@smart.local>
2023-10-24 09:25:38 +0300

    Commenting Out Relationship Fields

--- src/main/java/com/SpringJpa/Entities/Company.java
+++ src/main/java/com/SpringJpa/Entities/Company.java
@@ -1,9 +1,14 @@
 package com.SpringJpa.Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.GenerationType;
 @Entity
 public class Company {
@@ -24,6 +29,31 @@ public class Company {
     @Column
     private String contactInfo;
 
+    /* 
+    @OneToMany(mappedBy = "company",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductEntity> products = new ArrayList<>();
+
+    
+    @OneToMany(mappedBy = "company",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<InstitutionEntity> institutions = new ArrayList<>();
+    
+   public List<InstitutionEntity> getInstitution() {
+        return institutions;
+    }
+
+    public void setInstitution(List<InstitutionEntity> institution) {
+        this.institutions = institution;
+    }
+   
+    public List<ProductEntity> getProducts() {
+        return products;
+    }
+    
+
+    public void setProducts(List<ProductEntity> products) {
+        this.products = products;
+    }
+     */
     public Long getId() {
         return id;
     }
--- src/main/java/com/SpringJpa/Entities/InstitutionEntity.java
+++ src/main/java/com/SpringJpa/Entities/InstitutionEntity.java
@@ -1,10 +1,15 @@
 package com.SpringJpa.Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class InstitutionEntity {
@@ -22,6 +27,11 @@ public class InstitutionEntity {
     @Column
     private String createdAt;
 
+    /* 
+    @OneToMany(mappedBy = "institutions",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Company> companies = new ArrayList<>();
+
+    */
     public Long getId() {
         return id;
     }
