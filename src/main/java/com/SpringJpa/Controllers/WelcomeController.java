62eff619cf78890cbf118d631fc5b0859628efb7
moses wambui <moses.wambui@smart.local>
2023-10-25 21:33:07 +0300

    Application.property file - Add more application configuration properties for Database, Profile, Core, Docker etc. Modify Base url to read the application Name

--- src/main/java/com/SpringJpa/Controllers/WelcomeController.java
+++ src/main/java/com/SpringJpa/Controllers/WelcomeController.java
@@ -1,14 +1,17 @@
 package com.SpringJpa.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Value("${spring.application.name}")
+    private String name;
     @GetMapping("/")
     public String welcome() {
-        return "Welcome to Spring Data JPA";
+        return "Welcome to Spring Data JPA: "+name;
     }
     
 }
